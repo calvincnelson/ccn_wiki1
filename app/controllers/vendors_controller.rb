26520e1571d469762c49f89e7b392876f6e9f78f
Calvin Nelson <calvincnelson@gmail.com>
2017-04-24 09:29:39 -0600

    Initial Upload of Repository

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -70,6 +70,6 @@ class VendorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vendor_params
-      params.require(:vendor).permit(:vendor_name, :vendor_description, :vendor_rating)
+      params.require(:vendor).permit(:vendor_name, :vendor_description, :vendor_rating, :image)
     end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,6 @@
 class Vendor < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "600*300#" }, validate_media_type: false
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
